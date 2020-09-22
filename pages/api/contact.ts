c4b9f8792be27028ed519253ea9bac8d4a550833
Abdallah Geha <52007195+abdallahgeha@users.noreply.github.com>
2020-09-22 15:25:07 +0000

    serverless test

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import sgMail from "@sendgrid/mail";
+require("dotenv").config();
+const apiKey = process.env.SENDGRID_API_KEY!
+sgMail.setApiKey(apiKey);
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
+  const body = JSON.parse(req.body);
+  body.popo = 'sss'
+  res.status(200).json(body)
 }
